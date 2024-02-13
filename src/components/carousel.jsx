f4623c7805abf0b7fcee682591a28f2e7451ffd6
MShazilKhan7 <shazilk14@gmail.com>
2024-02-13 12:38:46 +0500

    new changes

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 import { FaLongArrowAltRight, FaLongArrowAltLeft } from "react-icons/fa";
 
